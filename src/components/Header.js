278688c02386f4fd2fa2576f7f66ffc3e1649e59
Ryan <louisalamone@gmail.com>
2020-03-09 21:39:06 -0600

    styling and functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,8 @@ export default class Header extends Component {
        <div className="text-info"><h1>Employee Directory</h1></div> 
         <br></br><br></br>
         <h2>Search Employees by Name, Email or Phone Number</h2>
+        <br></br>
+        <h3> Click on "Name" to Sort Order</h3>
         
       </div>
     )
